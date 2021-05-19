aacc24817be2330afaaf202ea7bddf6aef02a894
kelzerock <kelzzzer@gmail.com>
2021-05-19 21:07:58 +0300

    add: correct function - router work

--- i.js
+++ i.js
@@ -1,45 +1,47 @@
-
+let a = location.hash;
+changeArr(a);
 window.onpopstate = () => {
   let a = location.hash;
+  console.log(a)
   changeArr(a)
 }
 
- let but = document.querySelectorAll('button')
- but.forEach((i)=>{
-   i.addEventListener('click', function(){
-    let index = +i.innerHTML-1
+let but = document.querySelectorAll('button')
+but.forEach((i) => {
+  i.addEventListener('click', function () {
+    let index = +i.innerHTML - 1
     location.hash = index;
     changeArr(index)
-   }, false)
- })
+  }, false)
+})
 
-  function changeArr(number){
+function changeArr(number) {
   const arr = document.querySelectorAll('body div');
   let checkArr = []
-  for(let i=0; i<arr.length;i++){
-  checkArr.push('#'+i)
-  if(number==='#'+i || number===i ){
- 
-     arr.forEach((i)=>{
-      i.classList.add('start-no')
-      i.classList.remove('start')
-    });
-    arr[i].classList.add('start')
-    arr[i].classList.remove('start-no')
+  for (let i = 0; i < arr.length; i++) {
+    checkArr.push('#' + i)
+    if (number === '#' + i || number === i) {
+
+      arr.forEach((i) => {
+        i.classList.add('start-no')
+        i.classList.remove('start')
+      });
+      arr[i].classList.add('start')
+      arr[i].classList.remove('start-no')
+    }
   }
-  }
-  if(!checkArr.some((i)=>{
-    return i==='#'+number
-  })){
-    arr.forEach((i)=>{
+  if (!checkArr.some((i) => {
+    return i === '#' + number || i === number 
+  })) {
+    arr.forEach((i) => {
       i.classList.add('start-no')
       i.classList.remove('start')
     })
-  
-  
-let div = document.createElement('div');
-div.classList.add('start')
-div.innerHTML = 'This url page is incorrect'
-document.body.append(div)
-  } 
+
+
+    let div = document.createElement('div');
+    div.classList.add('start')
+    div.innerHTML = 'This url page is incorrect'
+    document.body.append(div)
+  }
 }
